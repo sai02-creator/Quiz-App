f66115224432012907d458187212409876087251
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-08-11 22:21:36 +1000

    Add options and add javascript for it to loop through the options

---
+++ src/components/quiz.jsx
@@ -0,0 +1,31 @@
+function Quiz () {
+    const questionBank = [
+        {
+            question: "What is the capital of France?",
+            options: ["Berlin", "London", "Paris", "Rome"],
+            answer: "Paris",
+        },
+        {
+            question: "Which language is used for web apps?",
+            options: ["PHP", "Python", "Javascript", "All"],
+            answer: "All",
+        },
+        {
+            question: "What does JSX stand for?",
+            options: ["JavaScript XML", "Java Syntax eXTENSION", "Just a simple example", "None of the above"],
+            answer: "JavaScript XML",  
+        },
+          
+    ];
+    return(
+        <div>
+            <h2> Question 1</h2> 
+            <p className="question">{questionBank[0].question}</p>
+            {questionBank[0].options.map((option) => (
+                <button className="option"> {option} </button>
+            ))}
+             </div>
+    );
+}
+
+export default Quiz;
